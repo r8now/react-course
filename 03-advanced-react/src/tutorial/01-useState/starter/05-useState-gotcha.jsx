78ed2a02d1c3993234f5b59610962d6367de536e
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-12 17:50:44 +0200

    useStates currentValue

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,20 @@
+import React from "react";
+import {useState} from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [add,setAdd] = useState(0);
+
+  return (
+    <div>
+      <h2>count is {add}</h2>;
+      <button className="btn" onClick={ () => setAdd((currentState)=> {
+        return currentState +1})}>
+        add 1 </button>
+      {console.log(add)}
+    </div>
+  );
 };
+
+
 
 export default UseStateGotcha;
