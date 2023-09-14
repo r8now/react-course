91a2211dac94d703e23cce32d53ad92945e6256d
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-14 23:45:16 +0200

    useEffect

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,15 @@
 const url = 'https://api.github.com/users';
+import { useEffect } from 'react';
+import {useState} from 'react';
 
 const FetchData = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    console.log("hello");
+  },[]);
+
+
   return <h2>fetch data example</h2>;
 };
 export default FetchData;
