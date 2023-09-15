1bf10dcab99ec2a38ec1e44973054ac08b51fd02
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-15 17:08:22 +0200

    .

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/02-useEffect/starter/04-fetch-data.jsx";
+import Starter from "./tutorial//03-conditional-rendering/starter/01-multiple-returns-basics.jsx";
 
 function App() {
   return (
--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -6,10 +6,50 @@ const FetchData = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(()=>{
-    console.log("hello");
+    const fetchData = async () =>{
+
+  try{
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users);
+
+  }catch (error){
+   console.log(error);
+} 
+
+
+  }
+  fetchData();
   },[]);
 
 
-  return <h2>fetch data example</h2>;
+  return <section>
+    <h3>github users</h3>
+    <ul className="users">
+      {users.map((user)=> {
+      console.log(user)
+
+        return(
+            <section>
+              <h3> githiub users</h3>
+              <ul className="users">
+                {users.map((user)=>{           
+                const { id, login, avatar_url, html_url } = user;
+                return <li key={id} >
+                  <img src={avatar_url} alt={login} />
+                  <div>
+                    <h5>{login}</h5>
+                    <a href={html_url}>profile</a>
+                  </div>
+                </li>
+}                
+                
+                )}
+              </ul>
+            </section>
+        )
+      })}
+    </ul>
+  </section>
 };
 export default FetchData;
