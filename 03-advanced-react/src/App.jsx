bc58e0074b4f50becefbf51375c6a2cc26ad66a3
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-17 08:55:10 +0200

    .

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial//03-conditional-rendering/starter/01-multiple-returns-basics.jsx";
+import Starter from "./tutorial//03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx";
 
 function App() {
   return (
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,56 @@
 import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
+
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [user,setUser] = useState(null);
+  const [isLoading,setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(()=>{
+
+    const fetchUser = async ()=>{
+      try{
+        const resp = await fetch(url);
+        if(!resp.ok){
+          setIsError(true);
+          setIsLoading(false);
+          return;
+        }
+        const user = await resp.json();
+        console.log(user)
+     
+       setUser(user);
+
+      }catch(error) {
+        setIsError(true);
+        console.log(error);
+      }
+      setIsLoading(false)
+    };
+    fetchUser();
+
+  },[])
+    
+  if (isLoading) {
+    return <h2>loading </h2>;
+  }
+ 
+  
+  if (isError) {
+    return <h2>error </h2>;
+  }
+  const {avatar_url, name} = user;
+
+  return (
+    <div>
+      <img style={{ width: "150px" }} src={avatar_url} alt={name} />
+      <h2>{name}</h2>
+    
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
+
+
+
