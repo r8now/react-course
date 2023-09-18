6f8d9c445e9f1db6baf1d40a19f55831c48c4478
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-18 11:41:27 +0200

    project structure

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,13 +1,16 @@
+import {Home, About} from "./tutorial/04-project-structure/starter/Pages";
 
-import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function.jsx";
+
+
 
 function App() {
 
 
   return (
     <div className="container">
-      <h2>Advanced React</h2>
-      <Starter />
+   <About/>
+   <Home/>
+     
     </div>
   );
 }
---
+++ 03-advanced-react/src/tutorial/04-project-structure/starter/Navbar/Navbar.jsx
@@ -0,0 +1,6 @@
+
+const Navbar = () => {
+  return <div id="navbarh2">Navbar</div>;
+};
+
+export default Navbar;
