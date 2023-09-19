9504ccfd0d60b8dc2e41b5c47f457f9cc5082248
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-19 23:35:34 +0200

    form- add new person

--- 03-advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,18 +1,33 @@
+import { useState } from "react";
+
 const ControlledInputs = () => {
+  const [name, setName]=useState("");
+  const [email, setEmail] = useState("");
+
+  const handleChange = (e) =>{
+    
+    setName(e.target.value);
+  }
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(name,email)
+  }
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <h4>controlled inputs</h4>
       <div className="form-row">
         <label htmlFor="name" className="form-label">
           name
         </label>
-        <input type="text" name="" id="name" className="form-input" />
+        <input type="text" name="" id="name" className="form-input" value={name} onChange={handleChange}/>
       </div>
       <div className="form-row">
         <label htmlFor="email" className="form-label">
           email
         </label>
-        <input type="text" name="" id="email" className="form-input" />
+        <input type="text" name="" id="email" value={email}
+       onChange={(e)=> setEmail(e.target.value)} className="form-input" />
 
       </div>
       <button type="submit" className="btn btn-block">submit</button>
--- 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
+import {data} from "../../../data"
+
 const UserChallenge = () => {
+  const [name, setName] = useState("");
+  const [users,setUsers] = useState([])
+
+  const handleSubmit =(e) => {
+    e.preventDefault();
+    if(!name) {
+      return;
+    }
+    const fakeId = Date.now();
+    const newUser = {id:fakeId, name};
+    const updatedUsers = [...users, newUser];
+    setUsers(updatedUsers);
+    console.log("form submitted")
+    setName("");
+
+  }
+
   return (
     <div>
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
         <h4>Add User</h4>
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input type='text' className='form-input'value={name} onChange={(e)=> setName(e.target.value)} id='name' />
         </div>
 
         <button type='submit' className='btn btn-block'>
@@ -15,6 +35,10 @@ const UserChallenge = () => {
         </button>
       </form>
       {/* render users below */}
+      <h4>users</h4>
+      {users.map((user)=>{
+        return <div key={user.id}> <h4>{user.name}</h4></div>
+      })}
     </div>
   );
 };
