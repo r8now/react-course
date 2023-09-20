b459deb0cd49527c58bf3279e2c0cb7d19882ddb
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-20 09:25:44 +0200

    handle multiple inputs

--- 03-advanced-react/src/tutorial/06-forms/starter/03-multiple-inputs.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/03-multiple-inputs.jsx
@@ -1,28 +1,48 @@
+import { useState } from "react";
+
 const MultipleInputs = () => {
+  const [users,setUsers]=useState({ 
+  name: "",
+   email: "",
+   password:""});
+
+   const handleChange = (e)=> {
+   
+    console.log(e.target.name)
+    console.log(e.target.value)
+    setUsers({...users, [e.target.name]:e.target.value})
+
+   }
+   const handleSubmit = (e) => {
+     e.preventDefault();
+    
+     console.log(users);
+   };
+  
   return (
     <div>
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
         <h4>Multiple Inputs</h4>
         {/* name */}
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input type='text' className='form-input' id='name'value={users.name} name="name" onChange={handleChange} />
         </div>
         {/* email */}
         <div className='form-row'>
           <label htmlFor='email' className='form-label'>
             Email
           </label>
-          <input type='email' className='form-input' id='email' />
+          <input type='email' className='form-input' id='email'value={users.email} onChange={handleChange} name="email"/>
         </div>
         {/* email */}
         <div className='form-row'>
           <label htmlFor='password' className='form-label'>
             Password
           </label>
-          <input type='password' className='form-input' id='password' />
+          <input type='password' className='form-input' id='password' value={users.password} name="password" onChange={handleChange}/>
         </div>
 
         <button type='submit' className='btn btn-block'>
