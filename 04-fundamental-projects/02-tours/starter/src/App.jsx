ec63f3707ad858f67ff4ae78ad838e75bc787553
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-20 18:17:07 +0200

    components added

--- 04-fundamental-projects/02-tours/starter/src/App.jsx
+++ 04-fundamental-projects/02-tours/starter/src/App.jsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
+
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [isLoading, setIsLoading] =useState(true);
+  const [tours, setTours] = useState([]);
+  
+  const fetchTours = async ()=> {
+    setIsLoading(true)
+    try{
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+      console.log(tours)
+    }catch(errors){
+console.log(errors)
+    }
+    setIsLoading(false);
+  }
+
+  useEffect(()=> {
+fetchTours();
+  },[])
+
+if(isLoading){
+  return <main>
+    <Loading/>
+  </main>
+}
+
+  return( 
+  
+  
+  <h2>hello</h2>
+  
+  );
 };
 export default App;
