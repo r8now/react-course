e998ea35bf7d69e3d03c7cdd144b1db93303cd96
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-20 22:52:06 +0200

    remove tours

--- 04-fundamental-projects/02-tours/starter/src/App.jsx
+++ 04-fundamental-projects/02-tours/starter/src/App.jsx
@@ -2,41 +2,46 @@ import { useEffect, useState } from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
 
-
-const url = 'https://course-api.com/react-tours-project';
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
-  const [isLoading, setIsLoading] =useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [tours, setTours] = useState([]);
-  
-  const fetchTours = async ()=> {
-    setIsLoading(true)
-    try{
+
+  const removeTour = (id) => {
+    const newTour = tours.filter((tour) => tour.id !== id);
+    setTours(newTour);
+  };
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
       const response = await fetch(url);
       const tours = await response.json();
       setTours(tours);
-      console.log(tours)
-    }catch(errors){
-console.log(errors)
+      console.log(tours);
+    } catch (errors) {
+      console.log(errors);
     }
     setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
   }
 
-  useEffect(()=> {
-fetchTours();
-  },[])
-
-if(isLoading){
-  return <main>
-    <Loading/>
-  </main>
-}
-
-  return( 
-  
-  
-  <main><Tours tours={tours} /></main>
-  
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
   );
 };
 export default App;
