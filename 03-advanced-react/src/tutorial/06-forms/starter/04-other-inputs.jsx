c546412d15cec5029cf66472cf68ff4f025e30fc
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-20 09:37:24 +0200

    checkbox and select

--- 03-advanced-react/src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react';
 const frameworks = ['react', 'angular', 'vue', 'svelte'];
 const OtherInputs = () => {
   const [shipping, setShipping] = useState(false);
+  const [framework, setFramework] = useState("react");
 
-  const handleShipping =(e)=> {
-console.log(e.target.checked)
-setShipping(e.target.checked)
-  }
+
+  const handleShipping = (e) => {
+    console.log(e.target.checked);
+    setShipping(e.target.checked);
+  };
+    const handleFramework = (e) => {
+     setFramework(e.target.value)
+    };
   return (
     <div>
       <form className='form'>
@@ -19,9 +24,15 @@ setShipping(e.target.checked)
           />
         </div>
         <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='framework' className='form-label'>
+          <label htmlFor='framework' className='form-label' value={framework} onChange={handleFramework}>
             Framework
           </label>
+          <select name="framework" id="framework">
+        {frameworks.map((framework)=>{
+          return <option key={framework}>{framework}</option>;
+        })}
+
+          </select>
         </div>
         <button type='submit' className='btn btn-block'>
           submit
