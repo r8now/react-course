1ec05b67acf5a279ca72ab55cb9d0fcf441ecdfe
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-22 16:43:53 +0200

    filter menu added

--- 04-fundamental-projects/05-menu/starter/src/App.jsx
+++ 04-fundamental-projects/05-menu/starter/src/App.jsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
 import Title from "./Title";
+import menu from "./data";
+import Menu from "./Menu";
+import Categories from "./Categories";
+/*
+const tempCategories = menu.map((item)=> item.category)
+const tempSet = new Set(tempCategories);
+const tempItems = ["all", ...tempSet]
+*/
+//one liner of the same thing
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+let me = ["hosein","moustafa"];
+let you = {name:"hosein obj",
+           after:"hello"};
 
 const App = () => {
+  const [menuItems,setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories)
+ const filterItems =(category)=> {
+  console.log(category);
+  if(category === "all"){
+    setMenuItems(menu);
+   
+  }else {
+    const newItems = menu.filter((item)=> item.category === category)
+    setMenuItems(newItems)
+
+  }
+
+
+  
+ }
+
   return <main>
     
     <section className="menu">
       <Title text="Our menu"/>
+      <Categories categories = {categories} filterItems={filterItems} />
+      <Menu items={menuItems}/>
     </section>
   </main>;
 };
