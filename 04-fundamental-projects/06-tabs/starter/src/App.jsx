8849795c4b464456681c8c267c8581ff9569eef3
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-25 14:51:53 +0200

    fetch data

--- 04-fundamental-projects/06-tabs/starter/src/App.jsx
+++ 04-fundamental-projects/06-tabs/starter/src/App.jsx
@@ -1,6 +1,23 @@
-const url = 'https://course-api.com/react-tabs-project';
+import { useEffect } from "react";
+import { useState } from "react";
+
+const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+  console.log(jobs);
+
+  useEffect(() => {
+fetchJobs();
+  }, []);
   return <h2>Tabs Starter</h2>;
 };
 export default App;
