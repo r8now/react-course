e132ce3708e3951ec1e74d749bf8be9913a9ad9d
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-25 19:18:45 +0200

    .

--- 04-fundamental-projects/07-slider/starter/src/Carousel.jsx
+++ 04-fundamental-projects/07-slider/starter/src/Carousel.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
-import {shortList,list,longList} from "./data"
-import { useState } from 'react'
+import React from "react";
+import { shortList, list, longList } from "./data";
+import { useState } from "react";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousel = () => {
-const [people, setPeople]=    useState(shirtList)
-  return (
-    <section className='slider-container'>
-        {people.map((person,index)=> {
-            const {id,img,name,title,quote} = person;
-            return <article className='slide' key={id}></article>
-        })}
-    </section>
-  )
-}
+  const [people, setPeople] = useState(shortList);
 
-export default Carousel
+  const prevSlide = () => {
+    console.log("hello");
+  };
+  const prevSlide = () => {
+    console.log("hello");
+  };
+
+  return (
+    <section className="slider-container">
+      {people.map((person, index) => {
+        const { id, image, name, title, quote } = person;
+        return (
+          <article className="slide" key={id}>
+            <img src={image} alt={name} className="person-img" />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+        );
+      })}
+      <button type="button" className="prev" onClick={prevSlide}>
+        <FiChevronLeft />
+      </button>
+      <FiChevronLeft />
+      <button type="button" className="next" onClick={nextSlide}>
+        <FiChevronRight />
+      </button>
+    </section>
+  );
+};
+
+export default Carousel;
