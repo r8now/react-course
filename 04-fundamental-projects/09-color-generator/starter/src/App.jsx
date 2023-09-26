5185465e2af42239b7fd8d83dc5621b74c4ca81b
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-26 17:59:48 +0200

    added componenets

--- 04-fundamental-projects/09-color-generator/starter/src/App.jsx
+++ 04-fundamental-projects/09-color-generator/starter/src/App.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import ColorList from "./ColorList";
+import Form from "./Form";
+import Values from "values.js"
+
 const App = () => {
-  return <h2>Color Generator Starter</h2>;
+  const [colors, setColors] = useState(new Values("#f15025").all(10));
+  console.log(colors)
+
+  return <main>
+    <Form/>
+    <ColorList/>
+  </main>;
 };
 export default App;
