cacdd05c7c0b168ba4d8cb245891f303d74f7aa4
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-26 14:57:11 +0200

    .

--- 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,31 @@
+import { useState } from "react";
+import data from "./data"
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count,setCount] =useState(1);
+const [text,setText] = useState([]);
+const handleSubmit = (e)=>{
+  e.preventDefault();
+  console.log(count);
+  let amount = parseInt(count);
+ setText(data.slice(0,amount));
+console.log(data);
+} 
+  return <section className="section-center">
+    <h4>tired of boring ipsum lorem</h4>
+    <form action="" className="lorem-form" onSubmit={handleSubmit}>
+      <label htmlFor="amount">Paragrafs</label>
+      <input type="number" name="amount" id="amount" min="1" step="1" max="8" value={count} onChange={(e)=>setCount(e.target.value)}/>
+      <button className="btn" type="submit">generate</button>
+    </form>
+
+    <article className="lorem-text">
+      {
+        text.map((item,index)=>{
+          return <p key={index}> {item}</p>
+        })
+      }
+    </article>
+  </section>;
 };
 export default App;
