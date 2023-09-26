97452446e0d34d76404b8f08c64e493b53a0b54f
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-26 18:17:46 +0200

    ToastContainer added

--- 04-fundamental-projects/09-color-generator/starter/src/App.jsx
+++ 04-fundamental-projects/09-color-generator/starter/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 import ColorList from "./ColorList";
 import Form from "./Form";
 import Values from "values.js"
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   const [colors, setColors] = useState(new Values("#f15025").all(10));
   console.log(colors)
+  toast.error("error")
 
   return <main>
     <Form/>
     <ColorList colors={colors}/>
+    <ToastContainer/>
   </main>;
 };
 export default App;
