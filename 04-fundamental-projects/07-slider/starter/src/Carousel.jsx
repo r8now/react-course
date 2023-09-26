d81355d0ab020cf5766dbaecc6f112b3880d64a6
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-26 10:15:08 +0200

    npm for slider installed with css

--- 04-fundamental-projects/07-slider/starter/src/Carousel.jsx
+++ 04-fundamental-projects/07-slider/starter/src/Carousel.jsx
@@ -1,25 +1,40 @@
 import React from "react";
-import { shortList, list, longList } from "./data";
 import { useState } from "react";
+import { shortList, list, longList } from "./data";
 import { FaQuoteRight } from "react-icons/fa";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousel = () => {
   const [people, setPeople] = useState(shortList);
+  const [currentPerson, setCurrentPerson] = useState(0);
 
   const prevSlide = () => {
-    console.log("hello");
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length;
+      return result;
+    });
   };
-  const prevSlide = () => {
-    console.log("hello");
+
+  const nextSlide = ()=> {
+    setCurrentPerson((oldPerson)=> {
+      const result = (oldPerson+1) % people.length;
+      return result;
+
+    })
   };
 
   return (
     <section className="slider-container">
-      {people.map((person, index) => {
+      {people.map((person, personIndex) => {
         const { id, image, name, title, quote } = person;
         return (
-          <article className="slide" key={id}>
+          <article
+            className="slide"
+            style={{
+              transform: `translateX(${100 * personIndex - currentPerson}%)`,
+            }}
+            key={id}
+          >
             <img src={image} alt={name} className="person-img" />
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>
@@ -31,7 +46,7 @@ const Carousel = () => {
       <button type="button" className="prev" onClick={prevSlide}>
         <FiChevronLeft />
       </button>
-      <FiChevronLeft />
+
       <button type="button" className="next" onClick={nextSlide}>
         <FiChevronRight />
       </button>
