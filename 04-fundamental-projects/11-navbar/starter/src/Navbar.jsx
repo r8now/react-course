a6e47196a695a8bdc361e7da056c67dc2eef5344
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-27 16:43:17 +0200

    .

--- 04-fundamental-projects/11-navbar/starter/src/Navbar.jsx
+++ 04-fundamental-projects/11-navbar/starter/src/Navbar.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {FaBars} from "react-icons/fa"
+import {links,social} from "./data"
+import logo from "./logo.svg"
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(true);
+  const toggleLinks = ()=> { return setShowLinks(!showLinks)};
+
   return (
-    <div>
-        
-    </div>
-  )
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+
+        <div
+          className={
+            showLinks ? "links-container show-container" :"links-container"
+          }
+        >
+          <ul className="links">
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar
