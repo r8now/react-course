c7276fac60d33541ceff24550d0a997237cb1431
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-10-02 15:19:00 +0200

    sidebar added

--- 04-fundamental-projects/12-sidebar/starter/src/Modal.jsx
+++ 04-fundamental-projects/12-sidebar/starter/src/Modal.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-  console.log(openSidebar);
-  console.log(openSidebar);
-  console.log(openSidebar);
-
+import { FaTimes } from "react-icons/fa";
+import {useGlobalContext} from "./context"
 
 const Modal = () => {
+ const {isModalOpen,closeModal} = useGlobalContext()
   return (
-    <div>Modal</div>
+    <div className="modal-overlay">
+      <div className="modal container">
+        <h3>modal content</h3>
+        <button className="close-modal-btn" onClick={closeModal}>
+          <FaTimes/>
+        </button>
+      </div>
+    </div>
   )
 }
 
