fb3f2648b79817845dd141031496a44ff2f5b9ee
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-11-21 16:49:10 +0100

    Add dark theme toggle functionality

--- 07-unsplash-images/starter/src/ThemeToggle.jsx
+++ 07-unsplash-images/starter/src/ThemeToggle.jsx
@@ -1,8 +1,18 @@
-
+import { useGlobalContext } from "./context"
+import {BsFillSunFill, BsFillMoonFill} from "react-icons/bs"
 const ThemeToggle = () => {
+  const {isDarkTheme, toggleDarkTheme} = useGlobalContext();
   return (
-    <div>ThemeToggle</div>
-  )
+    <section className="toggle-content">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <BsFillSunFill className="toggle-icon" />
+        ) : (
+          <BsFillMoonFill className="toggle-icon" />
+        )}
+      </button>
+    </section>
+  );
 }
 
 export default ThemeToggle
--- 07-unsplash-images/starter/src/context.jsx
+++ 07-unsplash-images/starter/src/context.jsx
@@ -3,6 +3,24 @@ import { createContext,useContext,useState,useEffect } from "react";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
+const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-    
+const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-theme", newDarkTheme);
+}
+
+    return (
+      <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+        {children}
+      </AppContext.Provider>
+    );
+  
+};
+
+  
+export const useGlobalContext = () => {
+  return useContext(AppContext);
 };
