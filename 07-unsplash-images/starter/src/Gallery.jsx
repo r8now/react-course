efe2175893d8e33419e9fc59efe9a64f675c9d83
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-11-23 12:35:18 +0100

    Add image search functionality to Gallery component

--- 07-unsplash-images/starter/src/Gallery.jsx
+++ 07-unsplash-images/starter/src/Gallery.jsx
@@ -1,8 +1,49 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const url =
+  "https://api.unsplash.com/search/photos?client_id=BFOFL32zWY1GxEYBU-ME5cL5qq_6jr906O-rNGfcqDI&query=office";
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+  console.log(response);
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <div>Loading...</div>
+      </section>
+    );
+  }
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <div>There was an error...</div>
+      </section>
+    );
+  }
+const results = response.data.results;
+if(results.length === 0){
+    return (
+      <section className="image-container">
+        <div>No results found...</div>
+      </section>
+    );
 }
+  return <section className="image-container">
+    {results.map((item)=>{
+      const url = item?.urls?.regular;
+      return <img src={url} alt={item.alt_description} key={item.id} className="img">
+      </img>
 
-export default Gallery
+    }
+    )}
+  </section>;
+};
+
+export default Gallery;
