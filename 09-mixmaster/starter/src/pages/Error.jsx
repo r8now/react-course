0df257dd5a52c4b353803f683f62ea0f58eb038a
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-11-28 22:30:01 +0100

    Add 404 page for not found error handling

--- 09-mixmaster/starter/src/pages/Error.jsx
+++ 09-mixmaster/starter/src/pages/Error.jsx
@@ -6,6 +6,18 @@ import img from "../assets/not-found.svg";
 const Error = () => {
   const error = useRouteError();
   console.log(error)
+  if(error.status === 404){
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="404" />
+          <h1>404</h1>
+          <h3>Sorry, the page you tried cannot be found</h3>
+          <Link to="/" className=""><h3>back home</h3></Link>
+        </div>
+      </Wrapper>
+    );
+  }
   return (
     <h1>Error</h1>
   )
